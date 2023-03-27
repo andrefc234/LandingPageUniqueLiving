7b58052e0dccdea0a4a13ad140b9dc9512f83b1b
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2023-03-27 12:08:22 -0600

    Update NavBar

--- components/NavB/index.tsx
+++ components/NavB/index.tsx
@@ -4,7 +4,7 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 export default function index() {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', backgroundColor: 'black' }}>
-      <Navbar variant="white" fixed="top" style={{ backgroundColor: 'rgba(191, 128, 107, 0.8)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
+      <Navbar variant="white" fixed="top" style={{ backgroundColor: 'rgba(191, 128, 107, 0.8)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }} expand='lg'>
         <div className="d-flex justify-content-start align-items-center">
           <Navbar.Brand href="#" >
             <img
@@ -13,6 +13,9 @@ export default function index() {
               width="139px"
               height="47px"
               className="d-inline-block align-top"
+              style={{
+                marginLeft:'5rem'
+              }}
             />
           </Navbar.Brand>
         </div>
@@ -21,10 +24,10 @@ export default function index() {
 
         <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
           <Nav className="mr-auto"></Nav>
-          <Nav className="justify-content-end">
-            <Nav.Link href="#about" className="text-white font-weight-bold">Servicios</Nav.Link>
-            <Nav.Link href="#services" className="text-white">Preventa</Nav.Link>
-            <Button variant='secondary' style={{ backgroundColor: '#EADC8C', outlineColor: '#EADC8C' }}>Contacto</Button>
+          <Nav className="justify-content-end" style={{marginRight:'5rem'}}>
+            <Nav.Link href="#about" className="text-white font-weight-bold pr-5">Servicios</Nav.Link>
+            <Nav.Link href="#services" className="text-white px-5">Preventa</Nav.Link>
+            <Button variant='secondary' className='px-5'style={{ backgroundColor: '#EADC8C', outlineColor: '#EADC8C' }}>Contacto</Button>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
