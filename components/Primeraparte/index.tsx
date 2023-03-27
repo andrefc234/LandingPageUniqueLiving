46f22e90a01eebd55df37b2a66689ef0bbeadcf2
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2023-03-27 17:13:50 -0600

    Update index.tsx

--- components/Primeraparte/index.tsx
+++ components/Primeraparte/index.tsx
@@ -1,37 +1,58 @@
-import { Container } from "react-bootstrap"
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Container, Carousel } from "react-bootstrap";
 
-export default function PrimeraParte(): JSX.Element {
+const StyledCarouselItem = styled(Carousel.Item)`
+  &&& {
+    img {
+      width: 100%;
+      height: 100vh;
+      object-fit: cover;
+    }
+  }
+`;
+
+
+
+
+export default function MyCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex: React.SetStateAction<number>, e: any) => {
+    setIndex(selectedIndex);
+  };
+  
+
   return (
-   
-    <div style={{ 
-      backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url("/assets/fA_Photo11.jpg")`, 
-      backgroundSize: 'cover', 
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      minHeight: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <div style={{ 
-        position: 'absolute', 
-        bottom: 0, 
-        left: 0,  
-        paddingLeft: '2rem', 
-        paddingBottom: '2rem',
-        display: 'flex', 
-        flexDirection: 'column', 
-        justifyContent: 'space-between', 
-        alignItems: 'flex-start',
-        height: '60vh'
-      }}>
-        <p className="text-white ml-3 h1 sm:text-3xl md:text-4xl" style={{ fontSize: '3em' }}>Arquitectura</p>
-        <p className="text-white ml-3 font-weight-bold leading-tight h3 sm:text-5xl md:text-6xl" style={{ fontSize: '4em' }}>
+    <Carousel activeIndex={index} onSelect={handleSelect} fade>
+      <StyledCarouselItem>
+        <img src="/assets/fA_Photo11.jpg" alt="First slide" />
+        <Carousel.Caption>
+        <p className="text-white ml-3  h1 sm:text-4xl md:text-5xl">Arquitectura</p>
+        <p className="text-white ml-3 font-weight-bold leading-tight  h3 sm:text-6xl md:text-7xl">
           Casa Cantera
         </p>
-        <p className="text-white ml-3 h6 sm:text-xl md:text-2xl" style={{ fontSize: '1.5em' }}>Santiago de Querétaro, Qro.</p>
-      </div>
-    </div>
- 
+        <p className="text-white  ml-3  h6 sm:text-2xl md:text-3xl">Santiago de Querétaro, Qro.</p>
+      </Carousel.Caption>
+      </StyledCarouselItem>
+      <StyledCarouselItem>
+        <img src="/assets/_28012018_DSC02202.jpg" alt="Second slide" />
+        <Carousel.Caption>
+       
+        <p  className="text-white ml-3  h1 sm:text-4xl md:text-5xl">Interiorismo</p >
+        <p>Santiago de Querétaro, Qro.</p>
+      </Carousel.Caption>
+      </StyledCarouselItem>
+      <StyledCarouselItem>
+        <img src="/assets/l1_Photo11.jpg " alt="Third slide" />
+        <Carousel.Caption>
+        <p className="text-white ml-3  h1 sm:text-4xl md:text-5xl">Arquitectura - Preventa</p>
+        <p className="text-white ml-3 font-weight-bold leading-tight  h3 sm:text-6xl md:text-7xl">
+        Loft
+        </p>
+        <p className="text-white  ml-3  h6 sm:text-2xl md:text-3xl">Santiago de Querétaro, Qro.</p>
+      </Carousel.Caption>
+      </StyledCarouselItem>
+    </Carousel>
   );
 }
